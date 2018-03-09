ef4ba770a0f1da5e2db530066b1bc524f3aa16fd
Silent-Era <junkyard@abv.bg>
2018-03-09 20:13:28 +0200

    adding firebase and redux-thunk to the app

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase'
+
+//insert your own firebase api keys here
+const config = {
+
+}
+
+firebase.initializeApp(config)
+
+const database = firebase.database()
+
+export {firebase, database as default}
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,12 @@
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import countReducer from '../reducers/count-reducer'
+import thunk from 'redux-thunk'
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
-    combineReducers({count: countReducer})
+    combineReducers({count: countReducer}),
+    composeEnhancers(applyMiddleware(thunk))
 )
 
 export default store
